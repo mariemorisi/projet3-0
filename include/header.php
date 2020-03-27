5b5defa8d4d81d944410bfeb563a41da537a9b58
morisi <m_morisi@yahoo.fr>
2020-03-27 14:35:48 +0100

    css + meta viewport

--- include/header.php
+++ include/header.php
@@ -1,38 +1,36 @@
 <?php
-// connexion a la bdd
-require("include/connecbdd.php")?>
+require("include/connecbdd.php");
+if(empty($_SESSION['id_users']) AND empty($_SESSION['pseudo']) AND empty($_SESSION['nom']) AND empty($_SESSION['prenom'])) 
+{
+    header('location: page_connexion.php');
+}
+else 
+{?>
 <!DOCTYPE html>
 <html lang="fr">
     <head>
         <meta charset="utf-8">
-        <?php if(!empty($title)){ ?>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <?php if(!empty($title)){?>
         <title><?= $title; }?></title>
         <link rel="stylesheet" type="text/css" href="css/style.css">
     </head>
     <body>
-        <header> 
+        <header>
             <a href="page_connexion.php">
                 <div id="logo">
                     <img class="logo" src="acteurs-partenaires/logo-gbaf.png" alt="logo">
                 </div>
             </a>
-            <div id="nomsession"> <!-- On affiche nom prénom session --> 
+            <div id="nomsession"><!-- On affiche nom prénom session --> 
                 <button class="bouton_nom">
-                    <?php 
-                    if(!empty($_SESSION['id_users']) AND !empty($_SESSION['pseudo']) AND !empty($_SESSION['nom']) AND !empty($_SESSION['prenom'])) 
-                    {
+                    <?php
                         echo '<img class="iconlog" src="acteurs-partenaires/iconlog.png" alt="iconelog"/> ' . $_SESSION['nom'] .' '. $_SESSION['prenom'] ;
-                        ?>
-                        </button>    
-                
-                        <button class="bouton_parametre" onclick= "window.location.href = 'parametres.php';"> Parametres du compte </button> 
-                        <button class="bouton_deconnexion" onclick= "window.location.href = 'page_deconnexion.php';"> Déconnexion </button> 
-                        <?php
-                    }
-                    else 
-                    {
-                        header('location: page_connexion.php');
-                    
+                    ?>
+                </button>    
+                    <button class="bouton_parametre" onclick= "window.location.href = 'parametres.php';"> Parametres du compte </button> 
+                    <button class="bouton_deconnexion" onclick= "window.location.href = 'page_deconnexion.php';"> Déconnexion </button> 
+                    <?php
                     }
                     ?>
             </div>
--- include/header_public.php
+++ include/header_public.php
@@ -2,6 +2,7 @@
 <html lang="fr">
 	<head>
 		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<?php if(!empty($title)){ ?>
 		<title><?= $title; }?></title>
 		<link rel="stylesheet" type="text/css" href="css/style.css">
